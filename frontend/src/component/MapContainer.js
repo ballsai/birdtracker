5e1a4cb9902d8d5c60e8ea3cda570deeaa3c44f3
ballxsai <jarunchairat.w@ku.th>
2020-03-15 22:55:41 +0700

    retreive google maps API

--- frontend/src/component/MapContainer.js
+++ frontend/src/component/MapContainer.js
@@ -1,15 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
 
-const MapContainer = () =>{
+// const MapContainer = () =>{
+//     const [birds, setBirds] = useState([]);
+
+//     useEffect(() => {
+//         async function fetchData(){
+//             try{
+//                 const res = await axios.get('/api/birds')
+//                 setBirds(res.data)
+//             } catch(error){
+//                 console.error(error.message);
+//             }
+//         }
+//         fetchData();
+//     }, []);
+
+//     return(
+//         <ul>
+//             { birds.map((bird, index)=> <li key={index.toString()}>{bird.bird_id}</li>) }
+//         </ul>
+//     );
+        
+// }
+
+const Map = () => {
     const [birds, setBirds] = useState([]);
 
-    useEffect(() => {
+    useEffect( () => {
         async function fetchData(){
             try{
-                const res = await axios.get('/api/birds')
+                const res = await axios.get('/api/birds');
                 setBirds(res.data)
-            } catch(error){
+            }catch(error){
                 console.error(error.message);
             }
         }
@@ -17,11 +41,28 @@ const MapContainer = () =>{
     }, []);
 
     return(
-        <ul>
-            { birds.map((bird, index)=> <li key={index.toString()}>{bird.bird_id}</li>) }
-        </ul>
+        <GoogleMap 
+            defaultZoom={18} 
+            defaultCenter={{lat:13.846246,lng:100.568640}} 
+        >
+            { birds.map((bird, index) => <Marker key = {index}
+                position = {{lat: Number(bird.lat), lng:Number(bird.lng)}}
+            />)}
+        </GoogleMap>
     );
-        
 }
 
-export default MapContainer;
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+export default function MapContainer(){
+    return (
+        <div style={{ width: "100vw", height: "100vh" }}>
+          <WrappedMap
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAD0zS55CxSt0oXdu8O230XgPF2_ZmKT7A`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+        </div>
+      );
+}
