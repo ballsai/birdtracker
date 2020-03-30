a5bf98f0992ed1fefe4ebe28b1a1c636cbf49950
ballxsai <jarunchairat.w@ku.th>
2020-03-30 23:32:36 +0700

    add backend gitignore

---
+++ backend/frontend/precache-manifest.4f34d6214b046c4c5dc685102e2f2460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd597de6415d3134a2325a35c29315c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "976fec8a47b9e493a2fe",
+    "url": "/static/css/main.4c3a91a7.chunk.css"
+  },
+  {
+    "revision": "de59c2c188bb3764486a",
+    "url": "/static/js/2.383205c6.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/static/js/2.383205c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976fec8a47b9e493a2fe",
+    "url": "/static/js/main.fa1cb1c9.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
